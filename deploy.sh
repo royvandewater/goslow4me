df38d5db5b58da0f07d7e21303321983c66c794a
Roy van de Water <dev+roy@integrumtech.com>
2014-05-11 07:52:20 -0700

    Deploy script works

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,6 @@ APP_DIR=/home/deploy/apps/goslow4me
 LOG_DIR=$APP_DIR/log
 CURRENT_DIR=$APP_DIR/current
 DESTINATION_DIR=$APP_DIR/releases/`date +%Y-%m-%d-%H-%M-%S`
-FOREVER_COMMAND="forever restart \
-  -l $LOG_DIR/forever.log \
-  -o $LOG_DIR/goslow4me.log \
-  -e $LOG_DIR/goslow4me.log \
-  -p $APP_DIR/forever
-  -c coffee $DESTINATION_DIR/src/application.coffee"
 
 function over_ssh_do(){
   ssh $HOST "$@"
@@ -28,8 +22,22 @@ function rsync_project(){
 
 over_ssh_do "mkdir -p $APP_DIR/releases $APP_DIR/log $APP_DIR/forever"
 rsync_project
-echo $FOREVER_COMMAND
-over_ssh_do "cd $DESTINATION_DIR && npm install && $FOREVER_COMMAND"
+over_ssh_do "cd $DESTINATION_DIR && npm install"
 over_ssh_do "ln -nsf $DESTINATION_DIR $CURRENT_DIR"
+echo "forever restart \
+  -l $LOG_DIR/forever.log \
+  -o $LOG_DIR/goslow4me.log \
+  -e $LOG_DIR/goslow4me.log \
+  -p $APP_DIR/forever \
+  -c coffee $CURRENT_DIR/src/application.coffee"
+
+over_ssh_do "forever restart \
+  -l $LOG_DIR/forever.log \
+  -o $LOG_DIR/goslow4me.log \
+  -e $LOG_DIR/goslow4me.log \
+  --append \
+  -p $APP_DIR/forever \
+  -c coffee $CURRENT_DIR/src/application.coffee"
+
 
 echo "Deployed."
