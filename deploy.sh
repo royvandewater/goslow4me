d27c043ccc008031addaaf330ab6b326d185b1eb
Roy van de Water <dev+roy@integrumtech.com>
2014-05-11 10:22:48 -0700

    Header layout for docs page

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,10 @@ CURRENT_DIR=$APP_DIR/current
 DESTINATION_DIR=$APP_DIR/releases/`date +%Y-%m-%d-%H-%M-%S`
 
 function over_ssh_do(){
-  ssh $HOST "$@"
+  COMMAND=$@
+  ssh $HOST "$COMMAND"
   if [ $? -ne 0 ]; then
+      echo "Failed to run: '$COMMAND'"
       exit 1
   fi
 }
@@ -20,16 +22,15 @@ function rsync_project(){
   fi
 }
 
+echo "creating directories"
 over_ssh_do "mkdir -p $APP_DIR/releases $APP_DIR/log $APP_DIR/forever"
+echo "starting rsync"
 rsync_project
+echo "npm install"
 over_ssh_do "cd $DESTINATION_DIR && npm install"
+echo "linking current"
 over_ssh_do "ln -nsf $DESTINATION_DIR $CURRENT_DIR"
-echo "forever restart \
-  -l $LOG_DIR/forever.log \
-  -o $LOG_DIR/goslow4me.log \
-  -e $LOG_DIR/goslow4me.log \
-  -p $APP_DIR/forever \
-  -c coffee $CURRENT_DIR/src/application.coffee"
+echo "Restarting Service"
 
 over_ssh_do "forever restart \
   -l $LOG_DIR/forever.log \
