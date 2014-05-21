907bfb0de979d55ca30c2b58c3374b9e44d75b19
Roy van de Water <dev+roy@integrumtech.com>
2014-05-21 13:50:26 -0700

    Add sitemap

--- deploy.sh
+++ deploy.sh
@@ -31,8 +31,8 @@ echo "npm install"
 over_ssh_do "cd $DESTINATION_DIR && npm install --production"
 echo "gzipping assets"
 over_ssh_do "cd $DESTINATION_DIR/public && \
-  for f in $(find .); do \
-    gzip -9 -c $f > $f.gz; \
+  for f in \$(find .); do \
+    gzip -9 -c \$f > \$f.gz; \
   done"
 echo "linking current"
 over_ssh_do "ln -nsf $DESTINATION_DIR $CURRENT_DIR"
