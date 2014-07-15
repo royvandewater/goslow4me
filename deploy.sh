8424c1377071255f2c6c7d27a5913827df09d9fe
Roy van de Water <dev+roy@integrumtech.com>
2014-07-14 19:18:11 -0700

    Use git clone strategy

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,21 @@
 start=`date +%s`
 
 HOST="deploy@goslow4.me"
+REPO="git@github.com:royvandewater/goslow4me"
 APP_DIR=/home/deploy/apps/goslow4me
 LOG_DIR=$APP_DIR/log
 CURRENT_DIR=$APP_DIR/current
 DESTINATION_DIR=$APP_DIR/releases/`date +%Y-%m-%d-%H-%M-%S`
+NPM_BIN="node_modules/.bin"
+
+function locally_do(){
+  COMMAND=$@
+  $COMMAND
+  if [ $? -ne 0 ]; then
+      echo "Failed to run: '$COMMAND'"
+      exit 1
+  fi
+}
 
 function over_ssh_do(){
   COMMAND=$@
@@ -25,19 +36,26 @@ function rsync_project(){
 
 echo "creating directories"
 over_ssh_do "mkdir -p $APP_DIR/releases $APP_DIR/log $APP_DIR/forever"
-echo "starting rsync"
-rsync_project
+
+echo "cloning git"
+over_ssh_do "git clone --depth=1 $REPO $DESTINATION_DIR"
+
 echo "npm install"
 over_ssh_do "cd $DESTINATION_DIR && npm install --production"
+
+echo "compiling remotely"
+over_ssh_do "cd $DESTINATION_DIR && $NPM_BIN/cake build"
+
 echo "gzipping assets"
 over_ssh_do "cd $DESTINATION_DIR/public && \
-  for f in \$(find .); do \
+  for f in \$(find . -type f); do \
     gzip -9 -c \$f > \$f.gz; \
   done"
+
 echo "linking current"
 over_ssh_do "ln -nsf $DESTINATION_DIR $CURRENT_DIR"
-echo "Restarting Service"
 
+echo "Restarting Service"
 over_ssh_do "forever restart \
   -l $LOG_DIR/forever.log \
   -o $LOG_DIR/goslow4me.log \
@@ -45,7 +63,6 @@ over_ssh_do "forever restart \
   --append \
   -p $APP_DIR/forever \
   -c coffee $CURRENT_DIR/src/application.coffee"
-
 
 end=`date +%s`
 runtime=$((end-start))
